bfe455249acf0e307e8dc072ae57745fc6d1b1eb
NGalygich <NGalygich@yandex.ru>
2022-05-24 05:16:35 +0300

    add registration

--- Program.cs
+++ Program.cs
@@ -2,39 +2,54 @@
 
 static class Program{
     public static void Main(){
-        using (UsersDataBaseContext db = new UsersDataBaseContext())
-        {
-            // создаем два объекта User
-            User Dmitriy = new User { name = "Dmitriy", password = "asdfg" };
-            //User Alisa = new User { name = "Alisa", password = "qaz123" };
-        
-            // добавляем их в бд
-            db.Users.Add(Dmitriy);
-            //db.Users.Add(Alisa);
-            db.SaveChanges();
-            Console.WriteLine("Объекты успешно сохранены");
-        
-            // получаем объекты из бд и выводим на консоль
-            var users = db.Users.ToList();
-            Console.WriteLine("Список объектов:");
-            foreach (User u in users)
-            {
-                Console.WriteLine($"{u.id}.{u.name} - {u.password}");
-            }
-        }
-
+        Authorization();
     }
     
     public static void Authorization(){
-
+        User user = new User();
+        Console.Write("Введите имя: ");
+        user.name = Console.ReadLine();
+        Console.Write("Введите пароль: ");
+        user.password = Console.ReadLine();
+        using (UsersDataBaseContext db = new UsersDataBaseContext()){
+            var users = db.Users.ToList();
+            foreach (var u in users){
+                if ((user.name == u.name) && (user.password == u.password)){
+                    Menu();
+                }
+            }
+            Console.WriteLine("Пользователь не найден. Зарегестрируйтесь.");
+            Registration(db);
+        }
     }
-    public static void Registration(){
-
+    public static void Registration(UsersDataBaseContext db){
+        User user = new User();
+        Console.Write("Придумайте имя: ");
+        user.name = Console.ReadLine();
+        Console.Write("Придумайте пароль: ");
+        user.password = Console.ReadLine();
+        var users = db.Users.ToList();
+        foreach (User u in users){
+            if (user.name == u.name){
+                Console.WriteLine("Имя пользователя уже занято. Придумайте другое.");
+                Console.ReadLine();
+                Console.Clear();
+                Registration(db);
+            }
+        }
+            db.Users.Add(user);
+            db.SaveChanges();
+            Console.WriteLine("Вы зарегестрированы.");
+            Menu();
     }
     public static void ChangeStatus(){
 
     }
     public static void WriteInfo(){
 
+    }
+    public static void Menu(){
+        Console.Write("Выберите действие.");
+        Console.ReadLine();
     }
 }
--- UsersDataBaseContext.cs
+++ UsersDataBaseContext.cs
@@ -3,29 +3,17 @@ namespace TestTask;
 
 public partial class UsersDataBaseContext : DbContext
 {
-    public UsersDataBaseContext()
-    {
-    }
-
+    public UsersDataBaseContext(){}
     public UsersDataBaseContext(DbContextOptions<UsersDataBaseContext> options)
-        : base(options)
-    {
-    }
-
+        : base(options){}
     public virtual DbSet<User> Users { get; set; } = null!;
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)  {
+        if (!optionsBuilder.IsConfigured){
             optionsBuilder.UseSqlite("Data Source=UsersDataBase.db");
         }
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
         OnModelCreatingPartial(modelBuilder);
     }
-
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
